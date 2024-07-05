3417a635268b445cc9d81d97bc8095b2bbd72c39
infirit89 <georgedautov808@gmail.com>
2024-07-05 14:43:53 +0300

    added create session endpoint

--- TravelChat.Server/Controllers/ChatController.cs
+++ TravelChat.Server/Controllers/ChatController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using TravelChat.Server.Models;
 
 namespace TravelChat.Server.Controllers
 {
@@ -12,13 +13,35 @@ namespace TravelChat.Server.Controllers
             _chatService = chatService;
         }
 
-
-        [HttpPost(nameof(SendMessage))]
-        public IActionResult SendMessage([FromBody] string message)
+        /// <summary>
+        /// Creates a new chat session
+        /// </summary>
+        /// <returns>The id to new session</returns>
+        /// <response code="201">Returns the new session id</response>
+        /// <response code="500">Returns a message saying what went wrong</response>
+        [HttpPost(nameof(CreateSession))]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public IActionResult CreateSession() 
         {
             try
             {
-                var response = _chatService.Message(message);
+                var sessionId = _chatService.CreateSession();
+                return StatusCode(StatusCodes.Status201Created, new { sessionId });
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, new { message = "Couldn't create a chat session" });
+            }
+        }
+
+
+        [HttpPost(nameof(SendMessage))]
+        public IActionResult SendMessage([FromBody]ChatMessage message)
+        {
+            try
+            {
+                var response = _chatService.Message(message.Content, message.SessionId);
                 _logger.LogInformation(response);
                 return Json(new { response });
             }
